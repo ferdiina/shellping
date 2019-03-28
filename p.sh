21dde8fecc3bfbbde4baf0011cfa050872ea07f5
Fred <flygg@163.com>
2019-03-28 15:08:42 +0800

    Add files via upload

---
+++ p.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ipok=0
+ipbad=0
+while read dstip
+do
+    if ping -c1 -W1 $dstip > /dev/null; then
+        ms=$(ping $dstip -c1 | grep time= | sed 's/=/ /g' | awk '{print $10}')
+        echo "$dstip Ping OK Legacy $ms ms"
+        let ipok++
+    else
+        echo "$dstip IP Boom"
+        let ipbad++
+    fi
+done < $1
+echo "totaly $ipok IP is fine"
+echo "totaly $ipbad IP is fail"
